3dd47b057c058be21218aad90fd65f887d071753
igar15 <igar15@yandex.ru>
2021-04-30 11:13:06 +0300

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ang-ecommerce'`, () => {
+  it(`should have as title 'angular-ecommerce'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ang-ecommerce');
+    expect(app.title).toEqual('angular-ecommerce');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ang-ecommerce app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-ecommerce app is running!');
   });
 });
